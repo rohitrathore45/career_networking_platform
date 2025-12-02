551b62e8866726e5b7354f2ecbeb63ab369b43bd
Rohit Rathore <rohitjeevanrathore@gmail.com>
2025-12-02 17:26:38 +0530

    completed the project

--- postsService/src/main/java/com/rohit/careerNetworkingPlatform/postsService/client/UploaderServiceClient.java
+++ postsService/src/main/java/com/rohit/careerNetworkingPlatform/postsService/client/UploaderServiceClient.java
@@ -1,14 +1,16 @@
 package com.rohit.careerNetworkingPlatform.postsService.client;
 
+import com.rohit.careerNetworkingPlatform.postsService.config.AppConfig;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(name = "uploader-service", path = "/uploads/file", url = "${UPLOADER_SERVICE_URI:}")
+@FeignClient(name = "uploader-service", path = "/uploads/file", url = "${UPLOADER_SERVICE_URI:}", configuration = AppConfig.class)
 public interface UploaderServiceClient {
 
-    @PostMapping
-    ResponseEntity<String> uploadFile(@RequestParam MultipartFile file);
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    ResponseEntity<String> uploadFile(@RequestPart("file") MultipartFile file);
 }
